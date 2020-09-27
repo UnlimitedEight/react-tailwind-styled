0671c411baaf39dd9759bd8c336ed257e818bd1e
Nick Frumusache <nick.frumusache@yahoo.com>
2020-09-27 22:22:02 +0300

    First version published

---
+++ src/app/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="mx-auto shadow rounded-lg flex justify-center w-48 mt-20 p-4 bg-red-100">
+      <h1 className="text-2xl font-medium">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
